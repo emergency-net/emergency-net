e5eb9fdb07e4607f3295661052e213c97b704684
cisel-zumbul <ciselzumbul09@gmail.com>
2023-11-25 19:58:20 +0300

    New db connection is added

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,9 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import sqlite3 from 'sqlite3';
+import {open} from "sqlite";
 import indexRouter from "./src/routes/index.js";
-
+import AppDataSource from "./database/newDbSetup.js";
 const app = express();
 
 // view engine setup
@@ -32,10 +33,23 @@ app.use((err, req, res, next) => {
   res.send(err);
 });
 
+let db;
+const func = async () => {
+   db = await open({
+    filename: 'database/Emergency-Net-DB.db',
+    driver: sqlite3.Database
+  })
+}
 
-const db = open({
-  filename: 'database/Emergency-Net-DB.db',
-  driver: sqlite3.Database
-})
+
+
+let sql = "SELECT * from  ap_private_keys";
+func().then(async () => {
+
+  console.log(await db.all(sql))
+  
+});
+
+
 
 export default app;
---
+++ backend/database/entities/User.js
@@ -0,0 +1,8 @@
+import { Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity
+export class User {
+    @PrimaryGeneratedColumn()
+    username
+
+}
