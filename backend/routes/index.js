94077fae41b70302d105c9ea0beedda05aba53b7
alperen-bircak <bircakalperen@gmail.com>
2023-11-25 13:37:54 +0200

    hello world page added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.send("<html><body><h1>Hello World!</h1></body></html>");
 });
 
 module.exports = router;
