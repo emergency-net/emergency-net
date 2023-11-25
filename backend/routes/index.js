c57b83c89f6f4614cb02787b15bbd23e0a809290
cisel-zumbul <ciselzumbul09@gmail.com>
2023-11-25 14:46:10 +0300

    Deletes old routes file

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("<html><body><h1>Hello World!</h1></body></html>");
-});
-
-module.exports = router;
