dcc393ff5e1943f088006424b18628d03b30e91a
alperen-bircak <bircakalperen@gmail.com>
2023-12-03 17:23:26 +0300

    changed send key logic

--- frontend/src/Services/register.tsx
+++ frontend/src/Services/register.tsx
@@ -1,9 +1,20 @@
+import { exportKey } from "@/Library/crypt";
 import axios from "axios";
 
-export async function register(body: { username: string; key: CryptoKey }) {
+export async function register({
+  key,
+  username,
+}: {
+  username: string;
+  key: CryptoKey;
+}) {
+  const exportedKey = exportKey(key);
   const response = await axios.post(
     import.meta.env.VITE_API_URL + "/register",
-    body
+    {
+      mtPubKey: exportedKey,
+      username,
+    }
   );
 
   return response.data;
