37d388ff2ab426f0ba845a8495059ea94049ea1d
alperen-bircak <bircakalperen@gmail.com>
2023-12-03 14:44:45 +0300

    added tailwind and shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import axios from "axios";
 import QueryProvider from "./Components/Providers/QueryClientProvider.tsx";
+import "./global.css";
 
 axios.defaults.headers.common = {
   "Content-Type": "application/json", // Sets content type to JSON
