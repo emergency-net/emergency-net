af324fe23a93ca5449a8d93e11e3de1480c98dac
alperen-bircak <bircakalperen@gmail.com>
2023-12-09 12:55:49 +0300

    removing keys

--- backend/src/util/genkeys.js
+++ backend/src/util/genkeys.js
@@ -1,7 +1,7 @@
 import crypto, { createPublicKey } from "crypto";
 import fs from "fs";
-
-const BASE_PATH = "../../";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { privateKey: privKey, publicKey: pubKey } = crypto.generateKeyPairSync(
   "rsa",
@@ -16,21 +16,20 @@ const { privateKey: adminPrivKey, publicKey: adminPubKey } =
   });
 console.log("KEYS GENERATED");
 fs.writeFileSync(
-  BASE_PATH + process.env.PUBLIC_KEY_PATH,
+  process.env.PUBLIC_KEY_PATH,
   pubKey.export({ format: "pem", type: "spki" })
 );
 fs.writeFileSync(
-  BASE_PATH + process.env.PRIVATE_KEY_PATH,
+  process.env.PRIVATE_KEY_PATH,
   privKey.export({ format: "pem", type: "pkcs8" })
 );
 
 fs.writeFileSync(
-  BASE_PATH + process.env.ADMIN_PUBLIC_KEY_PATH,
+  process.env.ADMIN_PUBLIC_KEY_PATH,
   adminPubKey.export({ format: "pem", type: "spki" })
 );
 fs.writeFileSync(
-  BASE_PATH + process.env.ADMIN_PRIVATE_KEY_PATH,
+  process.env.ADMIN_PRIVATE_KEY_PATH,
   adminPrivKey.export({ format: "pem", type: "pkcs8" })
 );
-
 console.log("KEYS SAVED");
--- backend/src/util/readkeys.js
+++ backend/src/util/readkeys.js
@@ -1,17 +1,10 @@
 import fs from "fs";
-const BASE_PATH = "../../";
 
-export const privateKey = fs.readFileSync(
-  BASE_PATH + process.env.PRIVATE_KEY_PATH
-);
-export const publicKey = fs.readFileSync(
-  BASE_PATH + process.env.PUBLIC_KEY_PATH
-);
-export const adminKey = fs.readFileSync(
-  BASE_PATH + process.env.ADMIN_PUBLIC_KEY_PATH
-);
+export const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH);
+export const publicKey = fs.readFileSync(process.env.PUBLIC_KEY_PATH);
+export const adminKey = fs.readFileSync(process.env.ADMIN_PUBLIC_KEY_PATH);
 export const adminPrivateKey = fs.readFileSync(
-  BASE_PATH + process.env.ADMIN_PRIVATE_KEY_PATH
+  process.env.ADMIN_PRIVATE_KEY_PATH
 );
 
 console.log("KEYS READ");
