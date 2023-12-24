8de2d82bde7d9bd5bd09c52e999c51b64b3aefd1
alperen-bircak <bircakalperen@gmail.com>
2023-12-24 16:42:23 +0300

    remove mkcert

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   server: {
     port: 3001,
   },
