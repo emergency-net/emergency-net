6ad2867ccb636033b0d797393facb9b808204c86
alperen-bircak <bircakalperen@gmail.com>
2024-01-07 15:21:14 +0300

    read keys change

--- backend/src/util/readkeys.js
+++ backend/src/util/readkeys.js
@@ -2,12 +2,37 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH);
-export const publicKey = fs.readFileSync(process.env.PUBLIC_KEY_PATH);
-export const adminPublicKey = fs.readFileSync(
-  process.env.ADMIN_PUBLIC_KEY_PATH
-);
-export const adminPrivateKey = fs.readFileSync(
-  process.env.ADMIN_PRIVATE_KEY_PATH
-);
-console.log("KEYS READ");
+let privateKey, publicKey, adminPublicKey, adminPrivateKey;
+
+function readKeys() {
+  privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH);
+  publicKey = fs.readFileSync(process.env.PUBLIC_KEY_PATH);
+  adminPublicKey = fs.readFileSync(process.env.ADMIN_PUBLIC_KEY_PATH);
+  adminPrivateKey = fs.readFileSync(process.env.ADMIN_PRIVATE_KEY_PATH);
+  console.log("KEYS READ");
+}
+
+// Initial read
+readKeys();
+
+// Function to re-read keys
+export function reReadKeys() {
+  readKeys();
+}
+
+// Export the keys as functions to ensure the latest values are returned
+export function getPrivateKey() {
+  return privateKey;
+}
+
+export function getPublicKey() {
+  return publicKey;
+}
+
+export function getAdminPublicKey() {
+  return adminPublicKey;
+}
+
+export function getAdminPrivateKey() {
+  return adminPrivateKey;
+}
