38cc9545981969100344f0254451f4216646d9eb
alperen-bircak <bircakalperen@gmail.com>
2024-01-07 14:18:00 +0300

    pu cert front

--- frontend/src/Library/cert.ts
+++ frontend/src/Library/cert.ts
@@ -11,36 +11,57 @@ export async function verifyApCert(cert: string): Promise<APData> {
 
   const splitCert = cert.split(".");
 
-  const content = base64ToJson(splitCert[0]);
+  const APcontent = base64ToJson(splitCert[0]);
+  const APsignature = splitCert[1];
+
   if (!adminKey) {
     return {
-      key: await importPublicKeyPem(content.apPub),
-      id: content.apId as string,
+      key: await importPublicKeyPem(APcontent.apPub),
+      id: APcontent.apId as string,
       type: "unknown",
     };
   }
   if (splitCert.length === 2) {
-    const signature = splitCert[1];
-
-    if (signature === "NO_CERT") {
+    if (APsignature === "NO_CERT") {
       return {
-        key: await importPublicKeyPem(content.apPub),
-        id: content.apId as string,
+        key: await importPublicKeyPem(APcontent.apPub),
+        id: APcontent.apId as string,
         type: "non_certified",
       };
     }
 
-    const stringContent = JSON.stringify(content);
+    const stringContent = JSON.stringify(APcontent);
 
-    const verified = await verify(adminKey, signature, stringContent);
+    const verified = await verify(adminKey, APsignature, stringContent);
     if (verified) {
       return {
-        key: await importPublicKeyPem(content.apPub),
-        id: content.apId as string,
+        key: await importPublicKeyPem(APcontent.apPub),
+        id: APcontent.apId as string,
         type: "admin_certified",
       };
     } else {
       throw new Error("AP Certificate Invalid");
+    }
+  } else if (splitCert.length === 4) {
+    const PUcontent = base64ToJson(splitCert[2]);
+    const PUsignature = splitCert[3];
+    const stringPUContent = JSON.stringify(PUcontent);
+    const PUverified = await verify(adminKey, PUsignature, stringPUContent);
+    if (!PUverified) {
+      throw new Error("PU Certificate in AP Certificate invalid.");
+    }
+
+    const stringAPContent = JSON.stringify(APcontent);
+
+    const verified = await verify(adminKey, APsignature, stringAPContent);
+    if (verified) {
+      return {
+        key: await importPublicKeyPem(APcontent.apPub),
+        id: APcontent.apId as string,
+        type: "pu_certified",
+      };
+    } else {
+      throw new Error("PU AP Certificate Invalid");
     }
   } else {
     throw new Error("AP Certificate Weird");
