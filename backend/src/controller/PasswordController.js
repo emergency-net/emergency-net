922c70428a9d947b15468c60f212526f175cf4ae
alperen-bircak <bircakalperen@gmail.com>
2024-01-11 21:04:58 +0300

    become pu done

--- backend/src/controller/PasswordController.js
+++ backend/src/controller/PasswordController.js
@@ -4,36 +4,25 @@ import { checkTod } from "../util/Util.js";
 
 class PasswordController {
   async getPassword(req, res) {
-    let tod_received = req.body.tod;
-    if (!checkTod(tod_received)) {
-      res.status(408).json({
+    // create one time password
+    const otp = generateOneTimePassword();
+    console.log("OTP is: ", otp);
+
+    if (otp != null && otp != "") {
+      res.status(200).json({
         id: apId,
         tod: Date.now(),
         priority: -1,
-        type: "MT_MSG_RJT",
-        error: "Timeout error.",
+        type: "MT_PSW_ACK",
       });
     } else {
-      // create one time password
-      const otp = generateOneTimePassword();
-      console.log("OTP is: ", otp);
-
-      if (otp != null && otp != "") {
-        res.status(200).json({
-          id: apId,
-          tod: Date.now(),
-          priority: -1,
-          type: "MT_PSW_ACK",
-        });
-      } else {
-        res.status(400).json({
-          id: apId,
-          tod: Date.now(),
-          priority: -1,
-          type: "MT_PSW_RJT",
-          error: "One time password could not be generated.",
-        });
-      }
+      res.status(400).json({
+        id: apId,
+        tod: Date.now(),
+        priority: -1,
+        type: "MT_PSW_RJT",
+        error: "One time password could not be generated.",
+      });
     }
   }
 }
